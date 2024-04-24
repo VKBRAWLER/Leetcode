2a955296060aaff4176ef0b9e8f25c18fd0cc21c
Varun Kharkwal <varunkh12345@gmail.com>
2024-04-24 22:42:47 +0530

    n-th-tribonacci-number

---
+++ March 2024/n-th-tribonacci-number.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public int tribonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    int[] arr = new int[n+1];
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 1;
+    int i = 3;
+    while (i < n+1) {
+      arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+      i++;
+    }
+    return arr[i-1];
+  }
+}
+class n {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.err.println(s.tribonacci(4));
+    System.err.println(s.tribonacci(25));
+  }
+}
