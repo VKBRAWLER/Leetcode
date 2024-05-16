d57c25f93c6183264eee3a7069a6d997085b9a23
Varun Kharkwal <varunkh12345@gmail.com>
2024-05-16 23:34:44 +0530

    evaluate-boolean-binary-tree

---
+++ May 2024/evaluate-boolean-binary-tree.java
@@ -0,0 +1,8 @@
+class Solution {
+  public boolean evaluateTree(TreeNode root) {
+      if (root.val == 1) return true;
+      else if (root.val == 2) return evaluateTree(root.left) || evaluateTree(root.right);
+      else if (root.val == 3) return evaluateTree(root.left) && evaluateTree(root.right);
+      return false;
+  }
+}
