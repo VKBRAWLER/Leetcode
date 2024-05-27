a989a512d21109d90f7e7db2d5ca5c162f620eb8
Varun Kharkwal <varunkh12345@gmail.com>
2024-05-27 13:13:34 +0530

    special-array-with-x-elements-greater-than-or-equal-x

---
+++ 2024 05/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int N = nums.size();
+        
+        vector<int> freq(N + 1, 0);
+        for (int i = 0; i < N; i++) {
+            freq[min(N, nums[i])]++;
+        }
+        
+        int numGreaterThanOrEqual = 0;
+        for (int i = N; i >= 1; i--) {
+            numGreaterThanOrEqual += freq[i];
+            if (i == numGreaterThanOrEqual) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
+int main() {
+  
+}
