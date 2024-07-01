03a5370a31c46bcc03182f3b7410098a840bd62d
Varun Kharkwal <varunkh12345@gmail.com>
2024-07-01 22:52:53 +0530

    greatest-common-divisor-of-strings

---
+++ 2024 07/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        // Check if they have non-zero GCD string.
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+
+        // Get the GCD of the two lengths.
+        int gcdLength = __gcd(str1.size(), str2.size());
+        return str1.substr(0, gcdLength);
+    }
+};
