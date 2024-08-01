c109b2089602ff54187b2f811a99319e4ef5b3bd
Varun Kharkwal <varunkh12345@gmail.com>
2024-08-01 07:19:15 +0530

    number-of-senior-citizens

---
+++ 2024 08/number-of-senior-citizens.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+      int count = 0;
+      for (int i = 0; i < details.size(); i++) {
+        if (stoi(details[i].substr(11, 2)) > 60) count++;
+      }
+      return count;
+    }
+};
