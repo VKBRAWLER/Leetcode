210b51cc016f3ab5228b5d41af143cc83cc6ae96
Varun Kharkwal <varunkh12345@gmail.com>
2024-08-07 10:14:03 +0530

    removing-stars-from-a-string

---
+++ 2024 08/removing-stars-from-a-string.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<int>st;
+        int n = s.size();
+        int i = 0;
+        while(i<n){
+            if(s[i] == '*'){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+            i++;
+        }
+        string ans="";
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();    
+        }
+        reverse(ans.begin(),ans.end());
+    return ans;
+
+    }
+};
