aae723864d15b317bf6fa140ae3c77936a2ecd1e
Varun Kharkwal <varunkh12345@gmail.com>
2024-08-28 22:58:49 +0530

    backspace-string-compare

---
+++ 2024 08/backspace-string-compare.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  bool backspaceCompare(string s, string t) {
+    int k=0,p=0;
+    for(int i=0;i<s.size();i++) {
+      if(s[i]=='#') {
+        k--;
+        k=max(0,k);
+      }
+      else {
+       s[k]=s[i];
+       k++;
+      }
+    }
+    for(int i=0;i<t.size();i++) {
+      if(t[i]=='#') {
+        p--;
+        p=max(0,p);
+      }
+      else {
+       t[p]=t[i];
+       p++;
+      }
+    }
+    if(k!=p) return false;
+    else {
+      for(int i=0;i<k;i++) {
+        if(s[i]!=t[i]) return false;
+      }
+      return true;
+    }
+  }
+};
+int main() {
+  string s = "ab", t = "ab";
+  bool a = s.compare(t) == 0;
+  printf("%d", a);
+}
